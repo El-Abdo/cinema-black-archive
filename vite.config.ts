675e99d5e7a4555575fc91760fec63c825a6eb84
El-Abdo <abdulrahman.muhammad2090@gmail.com>
2025-02-23 13:55:37 +0200

    added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     preact(),
     visualizer({ open: true }),
   ],
