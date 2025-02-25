66e76acaa5b7216c6ce04129571f9750d8a94d6e
El-Abdo <abdulrahman.muhammad2090@gmail.com>
2025-02-25 12:39:38 +0200

    film cards added to director card

--- src/pages/DirectorPage.tsx
+++ src/pages/DirectorPage.tsx
@@ -1,19 +1,42 @@
-import { useContext } from "preact/hooks";
+import { useContext} from "preact/hooks";
+import { useLocation } from "preact-iso";
 import { DataContext } from "../context/DataContext";
-import DirectorCard from "../components/DirectorCard";
+import FilmCard from "../components/FilmCard";
 import NotFound from "./NotFound";
 
-export default function DirectorPage(props: { id: number }) {
+export default function DirectorPage() {
+  const location = useLocation();
+  const directorId = parseInt(location.path.split("/").pop() || "0", 10);
   const data = useContext(DataContext);
   if (!data) return <p>Loading...</p>;
 
-  const director = data.directors[props.id];
+  const director = data.directors[directorId];
+  const films = director.films || {};
+  
   if (!director) return <NotFound/>;
 
   return (
-    <div>
-      <DirectorCard director={director} />
+    <>
+    <div class="p-6 flex flex-col lg:flex-row gap-6 items-start justify-end">
+            <img 
+                src={director.portrait_url} 
+                alt={director.name} 
+                class="w-40 h-40 object-cover rounded-full border border-gray-300 shadow-md"
+            />
+
+            <div class="flex-1 text-right">
+                <h1 class="text-2xl font-bold">{director.name}</h1>
+                <p class="text-gray-800 mt-6">{director.bio || "مخرج مصري."}</p>
+            </div>
     </div>
-  );
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {films.map((film) => (
+            <FilmCard key={film.id} film={film} />
+        ))}
+    </div>
+    </>
+    
+);
 }
 
