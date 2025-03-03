1937b86dd901dee9aa4d7fe7bf98ec1fe0c0dfd3
El-Abdo <abdulrahman.muhammad2090@gmail.com>
2025-03-04 00:02:02 +0200

    prepare for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
-import { visualizer } from 'rollup-plugin-visualizer';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     preact(),
-    visualizer({ open: true }),
   ],
 });
